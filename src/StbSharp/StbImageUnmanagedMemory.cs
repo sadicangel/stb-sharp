cb91d9a6f08e36732c756bcf3da52b8bde7a1892
sadicangel <sadicangel@hotmail.com>
2026-03-21 00:15:11 +0000

    Refactor Image to use unmanaged memory wrappers (#3)

---
+++ src/StbSharp/StbImageUnmanagedMemory.cs
@@ -0,0 +1,24 @@
+﻿using DotNext.Buffers;
+using DotNext.Runtime.InteropServices;
+using StbSharp.Interop;
+
+namespace StbSharp;
+
+internal sealed unsafe class StbImageUnmanagedMemory(byte* pixels, int length) : IUnmanagedMemory<byte>
+{
+    private byte* _pixels = pixels;
+
+    public void Dispose()
+    {
+        if (_pixels == null) return;
+        StbImage.stbi_image_free(_pixels);
+        _pixels = null;
+        Length = 0;
+    }
+
+    public Memory<byte> Memory => UnmanagedMemory.FromPointer(_pixels, Length);
+    public int Length { get; private set; } = length;
+
+    public Pointer<byte> Pointer => new(_pixels);
+    public Span<byte> Span => new(_pixels, Length);
+}
